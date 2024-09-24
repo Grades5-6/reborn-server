92d6b7414878e76258a23fa394d68d513716423c
yunuo46 <yunuo468@gmail.com>
2024-09-24 15:17:10 +0900

    init: spring boot project setup

---
+++ src/test/java/com/reborn/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reborn.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
