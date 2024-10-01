efc7b0d966dff22e72ec557b3313bc9f206e59f4
Chae-Yu <chaeyu2004@naver.com>
2024-10-01 15:28:18 +0900

    feat: jobPost domain 작성
    api 확정 후 수정 예정

--- src/main/java/com/reborn/server/domain/job/domain/JobPost.java
+++ src/main/java/com/reborn/server/domain/job/domain/JobPost.java
@@ -1,22 +1,23 @@
 package com.reborn.server.domain.job.domain;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
-import org.springframework.data.annotation.Id;
+import lombok.NoArgsConstructor;
 
 @Getter
+@Table(name = "job")
 @Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class JobPost {
 
     // api 받아오면 수정할 예정
-    @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(nullable = false)
+    @Id
     private Long id;
+
     private String title;
     private String content;
     private String company;
