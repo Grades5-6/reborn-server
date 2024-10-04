55659d48fe1ad795f47d792abaaf689f3fd16e88
Chae-Yu <chaeyu2004@naver.com>
2024-10-04 15:25:35 +0900

    feat: 일자리 공고가 중복되어 저장되지 않도록 확인하는 로직 구현
    저장 전, 현재 db에 중복된 일자리 데이터가 있는지 확인

--- src/main/java/com/reborn/server/domain/job/dao/JobPostRepository.java
+++ src/main/java/com/reborn/server/domain/job/dao/JobPostRepository.java
@@ -4,4 +4,5 @@ import com.reborn.server.domain.job.domain.JobPost;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface JobPostRepository extends JpaRepository<JobPost, Long> {
+    boolean existsByJobId(String jobId);
 }
