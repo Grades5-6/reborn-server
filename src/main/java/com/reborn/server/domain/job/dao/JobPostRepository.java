faea3719bd94c75103e19d0203a166cda71f58e9
Chae-Yu <chaeyu2004@naver.com>
2024-10-07 20:17:18 +0900

    feat: 일정 시간마다 자동으로 db에 저장된 정보가 갱신되도록 스케줄러 구현
    매일 정오에 해당 날짜를 기준으로 마감 접수 기간이 지난 공고는 DB에서 삭제하고 새로운 공고를 저장할 수 있도록 스케줄러 구현

--- src/main/java/com/reborn/server/domain/job/dao/JobPostRepository.java
+++ src/main/java/com/reborn/server/domain/job/dao/JobPostRepository.java
@@ -5,4 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface JobPostRepository extends JpaRepository<JobPost, Long> {
     boolean existsByJobId(String jobId);
+
+    void deleteByEndBefore(String string);
 }
