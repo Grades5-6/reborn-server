82cb8a198c83a8251bb55191c1552ab795d08f1a
Chae-Yu <chaeyu2004@naver.com>
2024-11-15 19:54:21 +0900

    feat: onboarding 정보 저장 필드 추가
    - 온보딩 화면에서 사용자에게 입력 받는 지역, 재직 여부, 관심사 정보 저장을 위한 필드 추가

--- src/main/java/com/reborn/server/domain/user/domain/User.java
+++ src/main/java/com/reborn/server/domain/user/domain/User.java
@@ -4,8 +4,6 @@ import com.reborn.server.domain.auth.domain.oauth.OauthProvider;
 import jakarta.persistence.*;
 import lombok.*;
 
-import java.time.LocalDateTime;
-
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
@@ -33,13 +31,26 @@ public class User {
     @Column(name = "profile_image")
     private String profileImg;
 
+    // 온보딩 화면
+    @Column(name="employment_status")
+    private String employmentStatus;
+
+    @Column(name="region")
+    private String region;
+
+    @Column(name="interested_field")
+    private String interestedField;
+
     @Builder
-    public User(String name, String email, OauthProvider oauthProvider, String introduce, String profileImg) {
+    public User(String name, String email, OauthProvider oauthProvider, String introduce, String profileImg, String employmentStatus, String region, String interestedField) {
         this.name = name;
         this.email = email;
         this.oauthProvider = oauthProvider;
         this.introduce = introduce;
         this.profileImg = profileImg;
+        this.employmentStatus = employmentStatus;
+        this.region = region;
+        this.interestedField = interestedField;
     }
 
     public static User of(String name, String email, OauthProvider oauthProvider, String profileImg) {
@@ -48,6 +59,14 @@ public class User {
                 .email(email)
                 .oauthProvider(oauthProvider)
                 .profileImg(profileImg)
+                .build();
+    }
+
+    public User updateOnboardingInfo(String employmentStatus, String region, String interestedField) {
+        return User.builder()
+                .employmentStatus(employmentStatus)
+                .region(region)
+                .interestedField(interestedField)
                 .build();
     }
 }
