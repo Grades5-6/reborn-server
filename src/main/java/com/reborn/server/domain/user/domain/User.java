aced96f7bc5e9d57ea813acaa4934bd64c52c929
Chae-Yu <chaeyu2004@naver.com>
2024-11-26 14:59:18 +0900

    feat: 일자리 온보딩 api 구현
    - User 도메인에 entity 추가
    - jwt -> 김영숙씨로 변경

--- src/main/java/com/reborn/server/domain/user/domain/User.java
+++ src/main/java/com/reborn/server/domain/user/domain/User.java
@@ -41,6 +41,16 @@ public class User {
     @Column(name="interested_field")
     private String interestedField;
 
+    // 일자리 온보딩 화면
+    @Column(name="sex")
+    private String sex; // 성별
+
+    @Column(name = "year")
+    private Integer year; // 태어난 연도
+
+    @Column(name="certificate")
+    private String certificate;
+
     @Builder
     public User(String name, String email, OauthProvider oauthProvider, String introduce, String profileImg, String employmentStatus, String region, String interestedField) {
         this.name = name;
@@ -66,5 +76,11 @@ public class User {
         this.employmentStatus = employmentStatus;
         this.region = region;
         this.interestedField = interestedField;
+    }
+
+    public void updateJobOnboardingData(String sex, int year, String certificate) {
+        this.sex= sex;
+        this.year=year;
+        this.certificate=certificate;
     }
 }
