eb4cdf0ba288f9d3d4810302616cfc0a08f781c7
Chae-Yu <chaeyu2004@naver.com>
2024-12-04 10:45:30 +0900

    feat: 직업 호출 서비스 로직 수정
    - 기간 지난 공고가 2페이지 이상 나올 시 api 호출 종료
    - save 저장 방식에서 saveAll 저장으로 수정

--- src/main/java/com/reborn/server/domain/job/application/JobPostService.java
+++ src/main/java/com/reborn/server/domain/job/application/JobPostService.java
@@ -1,11 +1,13 @@
 package com.reborn.server.domain.job.application;
 
+import com.reborn.server.domain.job.api.JobPostApi;
 import com.reborn.server.domain.job.dao.JobPostDetailRepository;
 import com.reborn.server.domain.job.dao.JobPostRepository;
 import com.reborn.server.domain.job.domain.JobPost;
-import com.reborn.server.domain.job.dto.JobPostDetailDto;
-import com.reborn.server.domain.job.dto.JobPostDto;
-import com.reborn.server.domain.job.api.ForecastApi;
+import com.reborn.server.domain.job.domain.JobPostDetail;
+import com.reborn.server.domain.job.dto.response.JobPostDetailDto;
+import com.reborn.server.domain.job.dto.response.JobPostDto;
+import com.reborn.server.domain.job.dto.response.JobResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -30,6 +32,7 @@ import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 @Service
@@ -39,16 +42,19 @@ public class JobPostService {
     private final JobPostRepository jobPostRepository;
     private final JobPostDetailRepository jobPostDetailRepository;
 
-    private static final Logger logger = LoggerFactory.getLogger(ForecastApi.class);
+    private static final Logger logger = LoggerFactory.getLogger(JobPostApi.class);
 
-
-    // DB에 jobPostDto 저장
+    // saveAll
     public void saveJobPosts(List<JobPostDto> jobPostDtos) {
-        for (JobPostDto jobDto : jobPostDtos) {
-            if (!jobPostRepository.existsByJobId(jobDto.getJobId())) {
-                JobPost jobPost = new JobPost(jobDto);
-                jobPostRepository.save(jobPost);
-            }
+        // 중복 제거 후 한 번에 저장
+        List<JobPost> jobPostsToSave = jobPostDtos.stream()
+                .filter(jobDto -> !jobPostRepository.existsByJobId(jobDto.getJobId())) // 중복 체크
+                .map(JobPost::new) // DTO를 엔티티로 변환
+                .collect(Collectors.toList());
+
+        // saveAll로 한 번에 저장
+        if (!jobPostsToSave.isEmpty()) {
+            jobPostRepository.saveAll(jobPostsToSave);
         }
     }
 
@@ -104,8 +110,8 @@ public class JobPostService {
         return null;
     }
 
-    // api 호출
-    public void syncJobData(RestTemplate restTemplate, String baseUrl, String serviceKey) throws Exception {
+    // 직업 api
+    public void callJobData(RestTemplate restTemplate, String baseUrl, String serviceKey) throws Exception {
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_XML);
         headers.setAccept(Collections.singletonList(MediaType.APPLICATION_XML));
@@ -115,10 +121,10 @@ public class JobPostService {
         messageConverters.add(stringConverter);
         restTemplate.setMessageConverters(messageConverters);
 
-
         int pageNo = 1;
         int numOfRows = 100;
         boolean hasMoreData = true;
+        int consecutiveEmptyPages = 0; // 연속으로 저장된 데이터가 없는 페이지 수
 
         while (hasMoreData) {
             String apiUrl = baseUrl + "%" + serviceKey + "&pageNo=" + pageNo + "&numOfRows=" + numOfRows;
@@ -136,9 +142,21 @@ public class JobPostService {
 
             // 받아온 데이터 파싱해서 saveJobPost 호출
             List<JobPostDto> jobPostDtos = jobPostApiParseXml(xmlData);
-            saveJobPosts(jobPostDtos);
 
-            // 더이상 저장할 페이지 정보 없으면 멈춤
+            if (jobPostDtos.isEmpty()) {
+                consecutiveEmptyPages++; // 저장된 데이터가 없으면 카운트 증가
+            } else {
+                saveJobPosts(jobPostDtos); // 데이터 저장
+                consecutiveEmptyPages = 0; // 저장된 데이터가 있으면 카운트 초기화
+            }
+
+            // 두 페이지 연속으로 저장된 데이터가 없으면 중단
+            if (consecutiveEmptyPages >= 2) {
+                logger.info("No new data for next two pages. Stopping API calls.");
+                break;
+            }
+
+            // 더 이상 저장할 페이지 정보가 없으면 멈춤
             int totalCount = getTotalCountFromXml(xmlData);
             if (pageNo * numOfRows >= totalCount) {
                 hasMoreData = false;
@@ -146,6 +164,7 @@ public class JobPostService {
                 pageNo++;
             }
         }
+
     }
 
     private int getTotalCountFromXml(String xmlData) throws Exception {
@@ -162,16 +181,91 @@ public class JobPostService {
             }
         }
 
+    // 마감 기한 안 지난 공고 필터링
     public void deleteExpiredJobPosts() {
         LocalDate today = LocalDate.now();
         jobPostRepository.deleteByEndBefore(today.toString());
     }
 
-    public List<JobPostDetailDto> getAllJobPosts() {
-        return jobPostDetailRepository.findAll()
-                .stream()
-                .map(JobPostDetailDto::from)
+    // 직업 상세 정보 파싱
+    public JobPostDetailDto getJobPostDetail(RestTemplate restTemplate, String detailUrl, String serviceKey, String jobId) throws Exception {
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_XML);
+        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_XML));
+
+        List<HttpMessageConverter<?>> messageConverters = new ArrayList<>();
+        StringHttpMessageConverter stringConverter = new StringHttpMessageConverter(StandardCharsets.UTF_8);
+        messageConverters.add(stringConverter);
+        restTemplate.setMessageConverters(messageConverters);
+
+        String apiUrl = detailUrl + "%" + serviceKey + "&id=" + jobId;
+        URI uri = new URI(apiUrl);
+
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+        ResponseEntity<String> response = restTemplate.exchange(uri, HttpMethod.GET, entity, String.class);
+        String xmlData = response.getBody();
+
+        xmlData = xmlData.trim(); // 앞뒤 공백 제거
+        xmlData = xmlData.replaceFirst("^([\\W]+)<", "<"); // BOM 제거
+
+        logger.info("Received XML Data: " + xmlData);
+
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        InputSource is = new InputSource(new StringReader(xmlData));
+        Document document = builder.parse(is);
+
+        NodeList item = document.getElementsByTagName("item");
+
+        if (item.getLength() > 0) {
+            Element itemElement = (Element) item.item(0);
+            String jobTitle = this.getElementValue(itemElement, "wantedTitle");
+            String hmUrl = this.getElementValue(itemElement, "homepage");
+            String companyName = this.getElementValue(itemElement, "plbizNm");
+            String workAddr = this.getElementValue(itemElement, "plDetAddr");
+
+            return JobPostDetailDto.builder()
+                    .jobTitle(jobTitle)
+                    .companyName(companyName)
+                    .workAddr(workAddr)
+                    .hmUrl(hmUrl)
+                    .build();
+        } else {
+            throw new Exception("No details for jobId: " + jobId);
+        }
+    }
+
+    // 직업 상세 정보 db에 저장
+    public void saveJobDetail(RestTemplate restTemplate, String serviceKey, String detailUrl) {
+        List<JobPost> jobList = jobPostRepository.findAll();
+        List<JobPostDetail> jobDetailsToSave = jobList.stream().map(jobPost -> {
+            try {
+                JobPostDetailDto jobDetailDto = getJobPostDetail(restTemplate, detailUrl, serviceKey, jobPost.getJobId());
+                return new JobPostDetail(jobPost.getJobId(), jobDetailDto);
+            } catch (Exception e) {
+                logger.error("Failed to fetch details for jobId");
+                return null;
+            }
+        }).filter(Objects::nonNull)
                 .collect(Collectors.toList());
+
+        if (!jobDetailsToSave.isEmpty()) {
+            jobPostDetailRepository.saveAll(jobDetailsToSave);
+            logger.info("job details saved successfully.");
+        } else {
+            logger.info("No job details to save.");
+        }
+    }
+
+    public List<JobResponseDto> getAllJobDetails(){
+        List<JobPostDetail> jobPostDetails = jobPostDetailRepository.findAll();
+        return  jobPostDetails.stream().map(jobPostDetail -> JobResponseDto.builder()
+                .workAddr(jobPostDetail.getWorkAddr())
+                .jobTitle(jobPostDetail.getJobTitle())
+                .hmUrl(jobPostDetail.getHmUrl())
+                .companyName(jobPostDetail.getCompanyName())
+                .build()).collect(Collectors.toList());
     }
 }
 
