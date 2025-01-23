b5afd16b6c114a1f6273f9dd261687c7db362369
Chae-Yu <chaeyu2004@naver.com>
2025-01-23 14:27:38 +0900

    feat: post에 comment 매핑
    - 하나의 포스트에 여러 개의 댓글
    - 포스트 로드 시 관련 댓글 로드
    - 포스트 삭제 시 관련 댓글 삭제

--- src/main/java/com/reborn/server/domain/community/domain/CommunityPost.java
+++ src/main/java/com/reborn/server/domain/community/domain/CommunityPost.java
@@ -1,12 +1,12 @@
 package com.reborn.server.domain.community.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.reborn.server.domain.comment.domain.Comment;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Entity
 @Getter
@@ -21,5 +21,11 @@ public class CommunityPost {
     private String content;
 
     private int likes_count = 0;
-    private int comments_count = 0;
+    // private int comments_count = 0;
+
+    // 하나의 포스트에 여러 개의 댓글
+    // 포스트 로드 될 때 관련 댓글도 로드됨 & 포스트 삭제 시 관련 댓글도 삭제
+    @OneToMany(mappedBy = "post", fetch = FetchType.EAGER, cascade = CascadeType.REMOVE)
+    @OrderBy("id asc") // 댓글 정렬
+    private List<Comment> comments;
 }
