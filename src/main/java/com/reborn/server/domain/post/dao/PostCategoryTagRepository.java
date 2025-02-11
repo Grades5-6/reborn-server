1bf62ea21386c9e202a414cfd60c2ae6d82012ce
yeonjy73 <yeon703@naver.com>
2025-02-11 22:02:27 +0900

    fix: post update fix

--- src/main/java/com/reborn/server/domain/post/dao/PostCategoryTagRepository.java
+++ src/main/java/com/reborn/server/domain/post/dao/PostCategoryTagRepository.java
@@ -7,13 +7,12 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface PostCategoryTagRepository extends JpaRepository<PostCategoryTag, Long> {
     @Query("SELECT pct.categoryTag FROM PostCategoryTag pct WHERE pct.post.id = :postId")
     List<CategoryTag> findCategoryTagsByPostId(Long postId);
 
     @Query("SELECT pct FROM PostCategoryTag pct WHERE pct.categoryTag = :categoryTag AND pct.post.id = :postId")
-    Optional<PostCategoryTag> findByCategoryTagAndPostId(@Param("categoryTag") CategoryTag categoryTag, @Param("postId") Long postId);
+    PostCategoryTag findByCategoryTagAndPostId(@Param("categoryTag") CategoryTag categoryTag, @Param("postId") Long postId);
 
 }
